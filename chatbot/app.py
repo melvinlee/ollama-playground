661a6b33e0bbe5b174ee0bf127a017ab31fc1de2
melvinlee <melvinlee@hotmail.com>
2025-02-10 20:04:06 +0800

    chat response using ollama.

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,3 +1,6 @@
+url = "http://localhost:11434"
+base_model = "llama3.2:3b"
+
 import streamlit as st
 from langchain_ollama import ChatOllama
 
@@ -9,3 +12,12 @@ with st.form("main-form"):
     text = st.text_input("Enter your text or question here:")
     submit = st.form_submit_button("Submit")
 
+def generate_response(text):
+    llm = ChatOllama(model=base_model, base_url=url, temperature=0)
+    response = llm.invoke(text)
+    return response.content
+
+if submit and text:
+    with st.spinner("Generating response..."):
+        response = generate_response(text)
+        st.write(response)
